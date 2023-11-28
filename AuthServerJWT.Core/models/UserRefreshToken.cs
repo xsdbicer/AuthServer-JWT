21e27da86f02f45f12a861d92b7a45d3e8dc0a2e
Şevval Dilara Biçer <41507884+sevvalbicerr@users.noreply.github.com>
2023-11-28 15:33:10 +0300

    Added models and generic repository

---
+++ AuthServerJWT.Core/models/UserRefreshToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServerJWT.Core.models
+{
+    public class UserRefreshToken
+    {
+        public string UserId { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime Expiration { get; set; }
+
+    }
+}
