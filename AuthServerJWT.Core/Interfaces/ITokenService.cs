9a53041497dbee5e1bf520cb08d6dea0d349fbcd
Şevval Dilara Biçer <41507884+sevvalbicerr@users.noreply.github.com>
2023-11-28 16:10:27 +0300

    Token service interface added

---
+++ AuthServerJWT.Core/Interfaces/ITokenService.cs
@@ -0,0 +1,17 @@
+﻿using AuthServerJWT.Core.Configuration;
+using AuthServerJWT.Core.Dtos;
+using AuthServerJWT.Core.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServerJWT.Core.Interfaces
+{
+    public interface ITokenService
+    {
+        TokenDto CreateToken(UserApp userApp);
+        ClientTokenDto CreateTokenByClient(Client client);
+    }
+}
