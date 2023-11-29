bac4856da6a91c25ea45ce566b61427882527aef
Şevval Dilara Biçer <41507884+sevvalbicerr@users.noreply.github.com>
2023-11-29 14:16:18 +0300

    User service added

---
+++ AuthServerJWT.Core/Dtos/CreateUserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServerJWT.Core.Dtos
+{
+    public class CreateUserDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
---
+++ AuthServerJWT.Core/Services/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using AuthServerJWT.Core.Dtos;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServerJWT.Core.Services
+{
+    public interface IUserService
+    {
+        Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<Response<UserAppDto>> GetUserByAsync(string userName);
+
+    }
+}
